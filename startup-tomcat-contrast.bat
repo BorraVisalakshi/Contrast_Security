8e92ba478b6b6cb11b9555d4428072d75e0a5add
BorraVisalakshi <55823754+BorraVisalakshi@users.noreply.github.com>
2021-05-25 11:08:32 +0530

    Update startup-tomcat-contrast.bat

--- startup-tomcat-contrast.bat
+++ startup-tomcat-contrast.bat
@@ -1,3 +1,3 @@
 set CATALINA_OPTS=%CATALINA_OPTS% -javaagent:%ProgramData%\Contrast\java\contrast.jar -Dcontrast.config.path=%ProgramData%\Contrast\java\contrast_security.yaml
 
-call C:\ProgramData\Contrast\java\apache-tomcat-9.0.43\apache-tomcat-9.0.43\bin\startup.bat
+call C:\ProgramData\Contrast\java\apache-tomcat-10.0.6\apache-tomcat-10.0.6\bin\startup.bat
